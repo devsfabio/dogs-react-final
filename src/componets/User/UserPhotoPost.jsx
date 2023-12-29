b1ff466d6e66a04d6e85c6cbccdd6446ad8b5764
Fabio Barbosa <fabiio403@gmail.com>
2023-12-29 19:12:08 -0300

    dogs-react-origamid

--- src/componets/User/UserPhotoPost.jsx
+++ src/componets/User/UserPhotoPost.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import styles from './UserPhotoPost.module.css';
 
 const UserPhotoPost = () => {
-
-function handleSubmit
+  function handleSubmit() {}
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
-      <form onSubmit={handleSubmit} ></form>
+      <form onSubmit={handleSubmit}></form>
     </section>
   );
 };
